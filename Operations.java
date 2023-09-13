9f641297148b67363cb7d178d12c6a19962c9b8e
Ifesinachi Nwatu <Ifesinachi.F.Nwatu-1@ou.edu>
2023-09-12 23:39:17 -0500

    Added a Main method

--- Operations.java
+++ Operations.java
@@ -1,11 +1,13 @@
 //a java program that computes basic operations of an array of numbers
 
+import java.util.Scanner;
+
 public class Operations {
 
     
         
         // add_list method: Adds a list of integers in a given array and returns the sum
-        public int add_list(int[] array){
+        public static int add_list(int[] array){
 
             int result = 0; //initialize result
 
@@ -21,7 +23,7 @@ public class Operations {
 
 
         //multp_list method: multiplies all numbers in a given array and returns the total
-        public int multp_list(int[] array){
+        public static int multp_list(int[] array){
 
             int result = 1; //initialize result
 
@@ -34,4 +36,37 @@ public class Operations {
         }
         //end
 
+
+
+        // main method to test the methods
+        public static void main(String[] args){
+
+            System.out.println("Type in any amount of integers separated by spaces: your first integer should represent the size of the array (it will not be included in the calculation)");
+            Scanner input = new Scanner(System.in); // create variable to read input from user
+
+            int size = input.nextInt(); // get the first integer which is the size of the list
+
+            int[] array = new int[size]; // declare array
+
+            //loop through the list and add all the numbers into the array
+            for (int i = 0; i < size; i++){
+
+                array[i] = input.nextInt(); // get each input
+            }
+
+            int sum_result = add_list(array); // call add_list method
+            int product_result = multp_list(array); // call the multp_list method
+
+            //print sum result
+            System.out.print("Addition result: ");
+            System.out.println(sum_result);
+
+            //print product result
+            System.out.print("Product Result: ");
+            System.out.println(product_result);
+
+        }
+
 }
+
+
