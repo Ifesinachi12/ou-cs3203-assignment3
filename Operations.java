17bc3c06c6fb6b0c6253a99aba08f854a2a47fa5
Ifesinachi Nwatu <Ifesinachi.F.Nwatu-1@ou.edu>
2023-09-12 22:52:11 -0500

    created add_list method

---
+++ Operations.java
@@ -0,0 +1,24 @@
+//a program that computes basic operations of an array of numbers
+
+public class Operations {
+
+    
+        
+        // add_list method: Adds a list of integers in a given array and returns the sum
+        public int add_list(int[] array){
+
+            int result = 0; //initialize result
+
+            for (int i = 0; i < array.length; i++){
+
+                result += array[i]; // sum all numbers within the array
+            }
+
+            return result; // return the sum total
+        } 
+        //end 
+    
+
+
+
+}
